fd48772b78fc494a09df61e47518faad62cfb14c
DeibyOd <deibyod@gmail.com>
2023-08-16 12:32:42 -0400

    Structure for new sections

---
+++ src/components/MemberVerification/MemberVerification.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Members() {
+  return (
+    <div>
+        <p>Las siguientes personas han sido participantes activ@s verificad@s en la historia de LocXué.</p>
+    </div>
+  );
+}
---
+++ src/components/Members/Members.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MemberVerification() {
+  return (
+    <div>
+        <p>Las siguientes personas han sido participantes activ@s verificad@s en la historia de LocXué.</p>
+    </div>
+  );
+}
